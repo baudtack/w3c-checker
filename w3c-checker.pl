dd081e1619a5ee701015861ae348caa09ac999f1
David Kerschner <docgnome@docgno.me>
2012-09-06 19:02:55 -0700

    added readme and license

--- w3c-checker.pl
+++ w3c-checker.pl
@@ -1,4 +1,20 @@
 #!/usr/bin/perl
+# w3c-checker - a program to download a web page and run it through the w3c-validator
+# Copyright (C) 2012 David Kerschner docgnome@docgno.me
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 use Modern::Perl;
 use LWP::UserAgent;
 use WebService::Validator::HTML::W3C;
